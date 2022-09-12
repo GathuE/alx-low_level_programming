c1fa0f539fc85b9c1bd3ccc74be082b29ceae1a4
Emmanuel Gathu <gathuimmanuel@gmail.com>
2022-09-11 22:26:44 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main function - Entry Point
+ * Return: Always 0 (Success)*/
+int main(void)
+{
+	/*Prints a statementusing printf */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
