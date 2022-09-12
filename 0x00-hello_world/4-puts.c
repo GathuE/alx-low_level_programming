957da746e44f73935de6d14b8d37cbda495a30fb
Emmanuel Gathu <gathuimmanuel@gmail.com>
2022-09-11 22:06:49 -0700

    C using Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(When Successful)
+ */
+int main(void)
+{
+	/*Printing a String using Puts*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
