20224b9013b48fd5bf87deb8bd9e8a6b46934e15
Emmanuel Gathu <gathuimmanuel@gmail.com>
2022-09-11 22:11:40 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program Entry 
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	/*Printing a System using Printf*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
