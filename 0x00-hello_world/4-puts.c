b0497d88dab3f4ea068c938432ec8baf382ac9ab
Emmanuel Gathu <gathuimmanuel@gmail.com>
2022-09-11 22:00:03 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		/*printing a string using puts*/
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+
+
+}
